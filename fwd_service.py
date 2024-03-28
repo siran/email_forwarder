88b4ae72c0c3605c6260256a073c8d00fea15639
An Rodriguez <anmichel@gmail.com>
2024-03-28 19:44:24 -0400

    manual changes

--- fwd_service.py
+++ fwd_service.py
@@ -8,10 +8,16 @@ from email.parser import BytesParser
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
-from email.utils import parseaddr, getaddresses
+from email.utils import parseaddr, getaddresses, formataddr
 
 s3 = boto3.client('s3')
 ses = boto3.client('ses', region_name='us-east-1')
+
+managed_domains = [
+    'preferredframe.com',
+    'wildnloyal.com',
+    'cinemestizo.com',
+]
 
 def get_rules():
     return {
@@ -19,7 +25,8 @@ def get_rules():
         'dani@': ['danielruiz2000@gmail.com'],
         '@preferredframe.com': ['anmichel@gmail.com'],
         '@wildnloyal.com': ['anmichel@gmail.com'],
-        '@cinemestizo.com': ['anmichel@gmail.com', 'danielruiz2000@gmail.com']
+        '@cinemestizo.com': ['anmichel@gmail.com', 'danielruiz2000@gmail.com'],
+        '_catch_all_': ['anmichel@gmail.com'],
     }
 
 def process_event(event, context=None):
@@ -41,79 +48,80 @@ def process_s3_event(s3_event):
     with open(local_filename, 'rb') as file:
         msg = BytesParser(policy=policy.default).parse(file)
 
-    rules = get_rules()
 
-    def extract_addresses(address_field):
-        return getaddresses(msg.get_all(address_field, []))
+    forwarding_to, forwarding_cc = apply_forwarding_rules(msg)
 
-    def update_forwarding_lists(address_list, forwarding_list):
-        for name, email in address_list:
-            domain = email.split('@')[-1]
-            for rule, forwards in rules.items():
-                if email.startswith(rule) or ('@' + domain) == rule:
-                    forwarding_list.update(forwards)
-
-    forwarding_to = set()
-    forwarding_cc = set()
-    forwarding_bcc = set()
-
-    addresses_list = [
-        extract_addresses('to'),
-        extract_addresses('cc'),
-        extract_addresses('bcc')
-    ]
-    forwarding_lists = [forwarding_to, forwarding_cc, forwarding_bcc]
-
-    for i, addresses in enumerate(addresses_list):
-        update_forwarding_lists(addresses, forwarding_lists[i])
-
-    if not any([forwarding_to, forwarding_cc, forwarding_bcc]):
-        print("No forwarding rules matched.")
-        return
-
-    send_response_email({
+    send_response_email(msg, {
         'to_addresses': list(forwarding_to),
         'cc_addresses': list(forwarding_cc),
-        'bcc_addresses': list(forwarding_bcc),
-        'reply_to_addresses': [parseaddr(msg['From'])[1]],
-        'subject': msg['Subject'],
-        'original_msg': msg,
-        'message_id': msg['Message-ID'],
-    })
+    }, original_recipient_domain=get_original_domain(msg))
 
-def send_response_email(params):
-    ses_from_address = "forwarder@preferredframe.com"
-    to_addresses = params['to_addresses']
-    cc_addresses = params.get('cc_addresses', [])
-    bcc_addresses = params.get('bcc_addresses', [])
-    reply_to_addresses = params['reply_to_addresses']
-    subject = params['subject']
-    original_msg = params['original_msg']
-    message_id = params['message_id']
+def apply_forwarding_rules(msg):
+    rules = get_rules()
+    forwarding_to = set()
+    forwarding_cc = set()
+    catch_all = rules.get('@', 'anmichel@gmail.com')
 
-    # Create a new MIME message
+    # Split handling of 'To' and 'Cc' to properly fill forwarding_cc
+    to_addresses = getaddresses(msg.get_all('to', []))
+    cc_addresses = getaddresses(msg.get_all('cc', []))
+
+    print(to_addresses, cc_addresses)
+
+    def apply_rules(addresses):
+        # Process addressess according to get_rules()
+        forwarding = set()
+        for name, email in addresses:
+            email_domain = email.split('@')[-1]
+            if email_domain not in managed_domains:
+                print('Skipping since not in managed domains')
+                continue # Skip non-managed domains
+            for rule, forward_email_list in rules.items():
+                if rule in email:
+                    print(f'match {rule} in {email}')
+                    forwarding.update(forward_email_list)
+                    break
+            else:
+                    forwarding.update(rules.get('_catch_all_'))
+
+        return forwarding
+
+    forwarding_to = apply_rules(to_addresses)
+    forwarding_cc = apply_rules(cc_addresses)
+
+    return forwarding_to, forwarding_cc
+
+def get_original_domain(msg):
+    # Default to a known domain if needed
+    recipient_address = parseaddr(msg['To'])[1]
+    return recipient_address.split('@')[-1]
+
+def send_response_email(original_msg, params, original_recipient_domain):
+    sender_name, sender_email = parseaddr(original_msg['From'])
+    if not sender_name:
+        sender_name = sender_email
+
+    # Sender address includes original recipient domain
+    ses_from_address = formataddr((f"{sender_name} via {original_recipient_domain}", f"fwdr@{original_recipient_domain}"))
+
     forward_msg = MIMEMultipart('mixed')
-    forward_msg['Subject'] = f"Fwd: {subject}"
+    forward_msg['Subject'] = original_msg['Subject']
     forward_msg['From'] = ses_from_address
-    forward_msg['To'] = ', '.join(to_addresses)
-    if cc_addresses:
-        forward_msg['Cc'] = ', '.join(cc_addresses)
-    forward_msg['Reply-To'] = ', '.join(reply_to_addresses)
-    forward_msg['In-Reply-To'] = message_id
-    forward_msg['References'] = message_id
+    forward_msg['To'] = ', '.join(params['to_addresses'])
+    if params.get('cc_addresses'):
+        forward_msg['Cc'] = ', '.join(params['cc_addresses'])
 
-    # Create a new MIME message part for the forwarded email body
-    forward_body = MIMEMultipart('alternative')
+    forward_msg['Reply-To'] = original_msg['Reply-To'] if 'Reply-To' in original_msg else original_msg['From']
+    forward_msg['In-Reply-To'] = original_msg['Message-ID']
+    forward_msg['References'] = original_msg['Message-ID']
 
-    # Add the original message as a MIME message part
-    forward_body.attach(original_msg)
+    att = MIMEBase('message', 'rfc822')
+    att.set_payload(original_msg.as_string())
+    encoders.encode_base64(att)
+    forward_msg.attach(att)
 
-    # Attach the forward body to the main message
-    forward_msg.attach(forward_body)
-
-    # Send the email
     ses.send_raw_email(
         Source=ses_from_address,
-        Destinations=to_addresses + cc_addresses + bcc_addresses,
+        Destinations=params['to_addresses'] + params.get('cc_addresses', []),
         RawMessage={'Data': forward_msg.as_string()}
     )
